d36ea5f7546eee2880fb6da258ee644b7c92684a
Temi-d <daratemitope@gmail.com>
2022-10-17 11:17:11 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,10 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success) 
+ * Return: Always 0 (Success)
  */
 
-int main(void) 
+int main(void)
 {
 
 	char My_str[] = "\"Programming is like building a multilingual puzzle";
