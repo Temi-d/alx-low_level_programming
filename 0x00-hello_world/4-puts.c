9382f6a249d992b6596b25f0b4769ebbff3366a5
Temi-d <daratemitope@gmail.com>
2022-10-17 10:44:10 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 
-int main() 
+/*main function*/
+int main(void) 
 {
 	/*String initialization*/
-	char My_str[] = "Programming is like building a multilingual puzzle";
+	char My_str[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(My_str);
-	return 0;
+
+	/*Return: always 0*/
+	return (0);
 }
