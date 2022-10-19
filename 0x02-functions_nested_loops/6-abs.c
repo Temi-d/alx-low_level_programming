58b9225a39617e68ad830e0be10a69b43984856c
Temi-d <daratemitope@gmail.com>
2022-10-19 14:49:14 -0700

    Abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _abs - Computes the absolute value of an integer
+ *
+ * @n: input number as an integer
+ *
+ * Rwturn: absolute integer
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (n * -1);
+	}
+}
+
