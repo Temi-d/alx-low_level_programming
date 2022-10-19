cb71d1741ec6ecac223949541d8dc972be992fc3
Temi-d <daratemitope@gmail.com>
2022-10-19 03:24:45 -0700

    PrintAlphabet10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * Print_alphabet_x10 - Prints 10 times the alphabeets in lowercase
+ * print_alphabet_x10 - Prints 10 times the alphabeets in lowercase
  *
  * Return:Always 0.
  */
