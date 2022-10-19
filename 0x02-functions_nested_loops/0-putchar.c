7e94644ad021f072adb1d49dd86c4ddbf06efd4b
Temi-d <daratemitope@gmail.com>
2022-10-19 02:40:51 -0700

    Putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -11,11 +11,10 @@ int main(void)
 	int i;
 	char c[] = "_putchar";
 
-	for(i=0; i<9; i++)
+	for (i = 0; i < 9; i++)
 		{
 		_putchar(c[i]);
 		}
-		_putchar('\n');	
-	
+		_putchar('\n');
 	return (0);
 }
