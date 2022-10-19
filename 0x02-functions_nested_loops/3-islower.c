4994d27e8b9c659382786dc8213254097c012c21
Temi-d <daratemitope@gmail.com>
2022-10-19 14:14:04 -0700

    isLower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,9 +4,9 @@
  * _islower - shows 1 if input isa lowercase letter.
  * shows 0 for other cases
  *
- * @c - The character in ASCII code
+ * @c: The character in ASCII code
  *
- *  Return 1 - for lowercase. 0 for the rest
+ *  Return: 1 for lowercase. 0 for the rest
  */
 
 int _islower(int c)
