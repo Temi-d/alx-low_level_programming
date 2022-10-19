e0353722afbcaaf7055f03b1a5c6e1e6d0e3dd8a
Temi-d <daratemitope@gmail.com>
2022-10-19 01:26:53 -0700

    Putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Entry point of function
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	
+	const char *ch = "_putchar\n";
+	putchar(*ch);
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This project takes a look at the concepts of nested while loops, C functions and C header files and how they are applied hands on.
