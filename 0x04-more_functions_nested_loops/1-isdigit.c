e2d81276fb7a9ecccded0fc846af769812812455
Temi-d <daratemitope@gmail.com>
2022-10-20 14:49:49 -0700

    isDigit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -3,20 +3,14 @@
 
 /**
  * _isdigit - checks for a digit (0 through 9)
- *
  * @c: Input variable
- *
- * Return: 1 if c is a digit. 0 if otherwise.
+ * Return: 1 if c is a digit. 0 if otherwise
  */
 
 int _isdigit(int c)
 {
 	if ((c >= 48) && (c >= 57))
-	{
 		return (1);
-	}
 	else
-	{
 		return (0);
-	}
 }
