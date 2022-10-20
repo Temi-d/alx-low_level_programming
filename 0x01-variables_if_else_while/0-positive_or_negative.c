b922169a8f81236704f267b4aa19ab4f03ed8eaf
Temi-d <daratemitope@gmail.com>
2022-10-19 17:30:52 -0700

    PosNeg

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -23,14 +23,15 @@ int main(void)
 					/* your code goes there */
 				if(n < 0)
 				{
-					printf("Number is negative\n");
+					printf("%d is negative\n", n);
 				}
 				else if (n > 0)
 				{
-					printf("Number is positive\n");
+					printf("%d is positive\n", n);
 				}
 				else
 				{
-					return (0);
+					printf("%d is zero\n", n);
 				}
+					return (0);
 }
