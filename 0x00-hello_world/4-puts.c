872f7cf51e0e73779f450444d513d521e3dc2d9f
Temi-d <daratemitope@gmail.com>
2023-02-18 04:37:04 -0800

    Comment

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #include<stdio.h>
+/**
+ * main - Entry point of code
+ * puts - Writes string to standard output
+ * Return: 0
+ */
+
 int main(void)
 {
-	char myStr[] = "\"Programming is like building a multilingual puzzle";
-	/**
-	 * Writing a string to standard output
-	 */
+	char myStr[] = "\"Programming is like building a multilingual puzzle"
 	puts(myStr);
-	return 0;
+	return (0);
 }
