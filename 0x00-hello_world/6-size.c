6cc328450d22f4718abf36fd52b70c263cea722f
Temi-d <daratemitope@gmail.com>
2023-02-19 04:27:43 -0800

    Size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point of code
+ * int - integers
+ * char - character literal
+ * float -floating point
+ * long int - Long integer
+ * long long int - Long long integer
+ */
+
+int main(void)
+{
+	char c;
+	int i;
+	long int li;
+	long long int lli;
+	float f;
+
+	printf("The size of a char is: %lu.\n", (unsigned long)sizeof(c));
+	printf("The size of an int is: %lu.\n", (unsigned long)sizeof(i));
+       printf("The size of a long int is: %lu.\n", (unsigned long)sizeof(li));
+       printf("The size of a long long int is :%lu.\n", (unsigned long)sizeof(lli));
+       printf("The size of a float is: %lu.\n", (unsigned long)sizeof(f));
+
+       return (0);
+}
+
