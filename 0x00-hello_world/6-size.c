28613a496a76401a44fb3d051bdb761258b36436
Temi-d <daratemitope@gmail.com>
2023-02-19 04:49:17 -0800

    Case

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -23,5 +23,5 @@ int main(void)
 	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(lli));
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 
-	Return (0);
+	return (0);
 }
