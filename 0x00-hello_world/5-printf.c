8c6e8b76117f9e3beb8b7611dc9a45b120dedc0e
Temi-d <daratemitope@gmail.com>
2023-02-19 04:05:35 -0800

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * main - Entry point of code
+ * printf - To pass a string literal
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
