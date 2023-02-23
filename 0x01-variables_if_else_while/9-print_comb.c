72a533b6a12abcc3379c3276dbb35f28ad8ac1df
Temi-d <daratemitope@gmail.com>
2023-02-23 05:15:42 -0800

    combo

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point of code
+ * Return: 0
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+	putchar(num + '0');
+	if (num < 9)
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	putchar('\n');
+	}
+
+	return (0);
+}
