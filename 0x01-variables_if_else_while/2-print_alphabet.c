15d8b85d643b9eef9f036951c11049887709e7b9
Temi-d <daratemitope@gmail.com>
2023-02-23 03:11:56 -0800

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point of code 
+ * Return: 0
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+		putchar('\n');
+
+	return (0);
+}
