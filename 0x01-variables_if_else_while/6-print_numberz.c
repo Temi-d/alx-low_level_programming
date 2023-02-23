014a9f981b328a67899366348b156ec99b868023
Temi-d <daratemitope@gmail.com>
2023-02-23 04:17:11 -0800

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point of code
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+	
+	for (i = '0'; i <= '9'; i++)
+	putchar(i);
+	putchar('\n');
+
+	return (0);
+}
