8eb8a017b6b59122120d256f7b700b4844e61f6a
Temi-d <daratemitope@gmail.com>
2023-02-23 04:39:41 -0800

    Reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point of code
+ * Return: 0
+ */
+
+int main(void)
+{
+	char bckwrds;
+
+	for (bckwrds = 'z'; bckwrds <= 'a'; bckwrds++)
+	putchar(bckwrds);
+	putchar('\n');
+
+	return (0);
+}
