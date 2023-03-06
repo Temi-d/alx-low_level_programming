26f303e405ac8e0d9472ce92e42768189af724dd
Temi-d <daratemitope@gmail.com>
2023-03-06 03:42:50 -0800

    10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x1- - Prints alphabets in lowercase 10 times
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	int i = 0, c;
+
+	while(i < 10)
+	{
+		c = 'a';
+
+		while(c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+
+		_putchar('\n');
+		i++;
+	}
+}
