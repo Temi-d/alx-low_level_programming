7b35b722c273d4881a1d6f6172dfb53acdba1106
Temi-d <daratemitope@gmail.com>
2023-03-06 04:27:04 -0800

    absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - Prints the absolute value of an integer
+ * @n: Input number, an integer
+ * Return: absolute value
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+	return (n);
+	}
+	else
+	{
+	return (n * -1);
+	}
+}
