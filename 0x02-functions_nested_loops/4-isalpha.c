728db796b2a6e85e70c03da3be8dccd6c6072423
Temi-d <daratemitope@gmail.com>
2023-03-06 04:03:48 -0800

    alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isalpha - Checks for uppercase letters
+ * @c - Character in ASCII code
+ * Return: 1 or 0
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+	_putchar('\n');
+}
